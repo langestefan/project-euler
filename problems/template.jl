680124d0e69d13c6b80b50d5982695e94309dad8
Stefan de Lange <langestefan@msn.com>
2023-11-22 03:10:55 +0100

    Add Project Euler problem template in Julia

---
+++ problems/template.jl
@@ -0,0 +1,26 @@
+# Project Euler Problem Template in Julia
+
+"""
+Problem X: Problem Title
+
+Published on: Day, DDth Month YYYY, hh:mm am/pm
+Solved by: <integer>
+Difficulty Rating: <X>%
+
+Problem Description:
+-------------------
+Problem statement goes here.
+
+Example:
+--------
+Example input and output.
+
+Solution:
+---------
+"""
+function solve_problem()
+    # Your solution code goes here
+end
+
+# Test the solution with the given example
+solve_problem()
